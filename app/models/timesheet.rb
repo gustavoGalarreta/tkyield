75ab00cb8ac417eb2e80eba1bdd3a79f90ad0c6e
PedroCarrillo <carrillochero6@gmail.com>
2015-02-10 14:08:19 -0500

    changing user related method from timesheet to user and timesheet controller

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -17,9 +17,6 @@ class Timesheet < ActiveRecord::Base
 	end
 
 	def start_timer
-		if has_a_timer_running?
-			cancel_active_timesheet
-		end
 		self.running = true
 		self.start_time = Time.now
 	end
@@ -36,31 +33,5 @@ class Timesheet < ActiveRecord::Base
   		# puts Time.at(self.total_time).utc.strftime("%H:%M")
    		return self.total_time
 	end
-
-	# obtain sum of all the time obtained in one day, maybe just front ? 
-	# add current user
-	def self.obtaining_total_time_per_day day
-		self.get_timesheet_per_day(day).sum(:total_time)
-	end
-
-	# query obtaining timesheet per day
-	#  add current user
-	def self.get_timesheet_per_day day
-		self.where(start_time: day.midnight..(day.midnight + 1.day))
-	end
-
-	# query obtaining the unique active timesheet
-	# add current user	
-	def get_timesheet_active
-		self.where(running: true).first
-	end
-
-	def has_a_timer_running?
-		get_timesheet_active != nil
-	end
-
-	def cancel_active_timesheet
-		get_timesheet_active.stop_timer
-	end
-
+	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,42 @@ class User < ActiveRecord::Base
   #
   # * *returns*
   #   - the admin user's full name
+
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def start_timer timesheet
+    if has_a_timer_running?
+      cancel_active_timesheet
+    end
+    timesheet.toggle_timer
+  end
+
+  def stop_timer timesheet
+    timesheet.toggle_timer
+  end
+
+  def obtaining_total_time_per_day day
+    Timesheet.get_timesheet_per_day(day).sum(:total_time)
+  end
+
+  def get_timesheet_per_day day
+    #ask about this created_at, start_time or create another field belongs_to_day
+    Timesheet.where(created_at: day.midnight..(day.midnight + 1.day), user_id: self.id)
+  end
+
+  def get_timesheet_active
+    Timesheet.where(running: true, user_id: self.id).first
+  end
+
+  def has_a_timer_running?
+    get_timesheet_active != nil
+  end
+
+  def cancel_active_timesheet
+    stop_timer get_timesheet_active
+  end
+
+
 end
