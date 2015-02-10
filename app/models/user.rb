0b7e6da31d626af70e7e8c3ed6760bd3b0305a2a
Billy Tandaypan <billy.tandaypan@tektonlabs.com>
2015-02-10 15:00:11 -0500

    devise cofirmation account set to false

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,10 @@ class User < ActiveRecord::Base
   has_many :user_projects
   has_many :projects, :through => :user_projects
 
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  def password_required?
-    super if confirmed?
-  end
-
-  def password_match?
-    self.errors[:password] << "can't be blank" if password.blank?
-    self.errors[:password_confirmation] << "can't be blank" if password_confirmation.blank?
-    self.errors[:password_confirmation] << "does not match password" if password != password_confirmation
-    password == password_confirmation && !password.blank?
+  def only_if_unconfirmed
+    pending_any_confirmation {yield}
   end
 
   # This function generates a string joining the admin user's first and last name.
