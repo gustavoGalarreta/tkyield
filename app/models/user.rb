f29f784a2d58bd940864f782db8a090da6087457
Billy Tandaypan <billy.tandaypan@tektonlabs.com>
2015-02-12 16:08:05 -0500

    user is confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :user_projects
   has_many :projects, :through => :user_projects
 
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   def only_if_unconfirmed
     pending_any_confirmation {yield}
