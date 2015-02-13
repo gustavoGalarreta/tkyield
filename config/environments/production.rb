f40d92b567c4b1efdddb6185cc99b2daffa569e8
Billy Tandaypan <billy.tandaypan@tektonlabs.com>
2015-02-13 10:43:06 -0500

    mailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,9 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
+   config.active_record.dump_schema_after_migration = false
+
+  #Mail configuration
    config.action_mailer.default_url_options = { :host => 'development.tektonlabs.com/tkyield_development' }
    ActionMailer::Base.smtp_settings = {
    :address => "smtp.gmail.com",
