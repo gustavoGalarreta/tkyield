d2a25f488d42f2720e398c590a4625b26e73bf43
RubyMobile <ruby3k@ip-10-141-134-146.ec2.internal>
2015-02-13 16:01:11 +0000

    syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_controller.relative_url_root = '/tkyield_development
+  config.action_controller.relative_url_root = '/tkyield_development'
 
   #Mail configuration
    config.action_mailer.default_url_options = { :host => 'development.tektonlabs.com/tkyield_development' }
