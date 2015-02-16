b78a12f9fc52a95e0654c8eb7efb6b3aaf5cfca0
Pedro Carrillo <pedro.carrillo@tektonlabs.com>
2015-02-16 10:15:12 -0500

    timesheet navigation per weeks

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -8,14 +8,8 @@ class TimesheetsController < ApplicationController
   	@start_of_week_day = @day_selected.beginning_of_week
     @timesheets = current_user.get_timesheet_per_day @day_selected
     @total_time_today = current_user.obtaining_total_time_per_day @day_selected
-    if current_user.projects.any?
-      @default_project = current_user.projects.first
-      @tasks = @default_project.tasks
-    else
-      @default_project = nil
-      @tasks = []
-    end
-    
+    @default_project = Project.all.first # current_user.projects.first
+    @tasks = @default_project.tasks
   end  
 
   # # GET /timesheets/new
