78d98d4d5f078c2a935efbc05be631a6d5a68a4f
Eduardo Arenas <eduardo.arenas@tektonlabs.com>
2015-02-26 10:52:57 -0500

    fixing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
-  resources :users , only: [:index, :new, :edit, :create, :update] do
+  resources :users, only: [:index, :new, :edit, :create, :update], path: "collaborators" do
     get 'show_user_project', to: 'users#projects', on: :member 
     patch 'update_user_project', to: 'users#update_projects', on: :member
     get 'resend_confirmation', to: 'users#resend_confirmation', on: :member
@@ -20,9 +20,9 @@ Rails.application.routes.draw do
     get 'toggle_timesheet', on: :member
   end
 
-  devise_for :users, :controllers => { :confirmations => 'confirmations', :registrations => 'registrations' }
+  devise_for :users, :controllers => { :passwords => 'user_device/passwords', :confirmations => 'user_device/confirmations' }
   devise_scope :user do
-    patch "/confirm" => "confirmations#confirm"
+    patch "/confirm" => "user_device/confirmations#confirm"
   end
 
 end
