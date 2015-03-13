74180ed342d85d08e8a9a57e7f3c04dfcb28768b
Billy Tandaypan <billy.tandaypan@tektonlabs.com>
2015-03-13 16:42:15 -0500

    deleted unused method

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,11 +2,6 @@ class Task < ActiveRecord::Base
   has_many :task_projects
   has_many :projects, :through => :task_projects
   has_many :timesheets
-
   validates :name, presence: true
-
-  def total_time beginning, ending
-    Timesheet.where(belongs_to_day: beginning..ending,task_id: self.id).sum(:total_time)
-  end
   
 end
