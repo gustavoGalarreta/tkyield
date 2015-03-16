b9449c21ccd0738a823c3d20e42bb802f7214a9a
Billy Tandaypan <billy.tandaypan@tektonlabs.com>
2015-03-16 17:01:56 -0500

    bugs fixed

--- app/helpers/time_stations_helper.rb
+++ app/helpers/time_stations_helper.rb
@@ -1,2 +1,10 @@
 module TimeStationsHelper
+
+	def days_between_dates(beginning, ending)
+		i =  (ending - beginning).to_i
+	    days_of_week = []
+	    (0..i).each{ |index| days_of_week << beginning + index.days }
+	    return days_of_week
+	end
+
 end
