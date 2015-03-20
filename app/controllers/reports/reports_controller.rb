4c2a6aee6d5f270eae4eb9fca95df1f5e2de40ea
Billy Tandaypan <billy.tandaypan@tektonlabs.com>
2015-03-20 16:53:10 -0500

    added filter by team in collaborators report

--- app/controllers/reports/reports_controller.rb
+++ app/controllers/reports/reports_controller.rb
@@ -12,9 +12,13 @@ module Reports
       add_breadcrumb "Reports", :reports_list_path
       add_breadcrumb "Timesheet Report", :reports_dash_path
       @clients = Client.order("name")
-      @users = User.order("first_name, last_name")
+      if !params[:teamId].blank?
+        @users= User.where(team_id: @selected_team).order("first_name, last_name")
+      else 
+        @users = User.order("first_name, last_name")
+      end
       @projects = Project.includes(:client).order("name")
-      @timesheet = Timesheet.where(belongs_to_day: @beginning..@end).includes(:task,:user,project: [:client])      
+      @teams = Team.order("name")
     end
 
     def clients_excel
@@ -32,7 +36,13 @@ module Reports
     end
 
     def collaborators_excel
-      @timesheet = Timesheet.where(belongs_to_day: @beginning..@end).includes(:task,:user,project: [:client]).order("belongs_to_day ASC").order("users.first_name, users.last_name")
+      if !params[:teamId].blank?
+        @users= User.where(team_id: @selected_team).order("first_name, last_name")
+        @timesheet = Timesheet.where(belongs_to_day: @beginning..@end, user: @users).includes(:task,:user,project: [:client]).order("belongs_to_day ASC").order("users.first_name, users.last_name")
+      else 
+        @timesheet = Timesheet.where(belongs_to_day: @beginning..@end).includes(:task,:user,project: [:client]).order("belongs_to_day ASC").order("users.first_name, users.last_name")
+      end
+
       respond_to do |format|
         format.xlsx
       end
@@ -52,6 +62,7 @@ module Reports
         @beginning = @day_selected.at_beginning_of_month
         @end = @day_selected.at_end_of_month
       end
+      @selected_team = Team.find(params[:teamId]) if !params[:teamId].blank?
     end
   end
 end
