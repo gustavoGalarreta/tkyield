102a67a059c6b75b87bd620e260e8b384141904c
Billy Tandaypan <billy.tandaypan@tektonlabs.com>
2015-04-01 12:16:26 -0500

    web service for on_time

--- app/controllers/api/v1/time_stations_controller.rb
+++ app/controllers/api/v1/time_stations_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::TimeStationsController < Api::ApiV1Controller
 
 	def create
-		@user = User.find_by(params[:qr_code]||params[:pin_code])
+		@user = User.find_by(user_params)
 		@last_time_station = TimeStation.where(user: @user).last
 	    @is_in = true
 	    @out_time = nil
 	    @in_time = nil
+	    @total_time = nil
+	    @success = true
 	    if @user
 			if @last_time_station.nil? or !@last_time_station.parent_id.nil?
 				a = TimeStation.create(user_id: @user.id)
@@ -14,9 +16,17 @@ class Api::V1::TimeStationsController < Api::ApiV1Controller
 				a = TimeStation.create(user_id: @user.id, parent_id: @last_time_station.id, total_time: Time.zone.now - @last_time_station.created_at )
 				@in_time = @last_time_station.created_at
 				@out_time = a.created_at
+				@total_time =  a.total_time
 				@is_in = false
 			end
+		else
+	    	@success = false
 	    end
+	end
+
+  private
+  	def user_params
+		params.permit(:qr_code, :pin_code)
 	end
 
 end
