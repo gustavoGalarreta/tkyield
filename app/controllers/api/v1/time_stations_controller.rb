cfa9be7f0fedc50f28a177bf443f0f0b7535352c
Billy Tandaypan <billy.tandaypan@tektonlabs.com>
2015-04-01 11:58:48 -0500

    web service for on_time

--- app/controllers/api/v1/time_stations_controller.rb
+++ app/controllers/api/v1/time_stations_controller.rb
@@ -1,15 +1,22 @@
 class Api::V1::TimeStationsController < Api::ApiV1Controller
 
 	def create
-		@user = User.find_by_qr_code(params[:qr_code])
+		@user = User.find_by(params[:qr_code]||params[:pin_code])
 		@last_time_station = TimeStation.where(user: @user).last
-		@is_in = true
-		if @last_time_station.nil? or !@last_time_station.out_time.nil?
-			TimeStation.create(user_id: @user.id, in_time: Time.zone.now)
-		elsif @last_time_station.out_time.nil?
-			@last_time_station.update(out_time: Time.zone.now, total_time: Time.zone.now - @last_time_station.in_time)
-			@is_in = false
-		end
+	    @is_in = true
+	    @out_time = nil
+	    @in_time = nil
+	    if @user
+			if @last_time_station.nil? or !@last_time_station.parent_id.nil?
+				a = TimeStation.create(user_id: @user.id)
+				@in_time = a.created_at
+			elsif @last_time_station.parent_id.nil? 
+				a = TimeStation.create(user_id: @user.id, parent_id: @last_time_station.id, total_time: Time.zone.now - @last_time_station.created_at )
+				@in_time = @last_time_station.created_at
+				@out_time = a.created_at
+				@is_in = false
+			end
+	    end
 	end
 
 end
