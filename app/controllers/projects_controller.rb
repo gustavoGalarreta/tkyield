a9ddf2fb82b7b38eebec6ed3382e82bbad11aea9
Eduardo Arenas <eduardo.arenas@tektonlabs.com>
2015-04-23 16:30:28 -0500

    fix timesheet reports

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -77,7 +77,7 @@ class ProjectsController < DashboardController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = Project.find(params[:id])
+      @project = current_account.projects.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/controllers/reports/user_project_controller.rb
+++ app/controllers/reports/user_project_controller.rb
@@ -4,13 +4,12 @@ module Reports
     add_breadcrumb "Dashboard", :dashboard_path 
     add_breadcrumb "Reports", :reports_list_path 
     add_breadcrumb "Timesheet Report", :reports_dash_path
-    
-    
+
     def show
       add_breadcrumb "Collaborator - Project", :reports_user_project_path
-      @user = User.find params[:user_id]
-      @project = Project.find params[:project_id]
-      @timesheets = Timesheet.where(belongs_to_day: @beginning..@end,user_id: @user.id, project_id: @project.id).includes(:task)
+      @user = current_account.users.find params[:user_id]
+      @project = current_account.projects.find params[:project_id]
+      @timesheets = Timesheet.where(belongs_to_day: @beginning..@end, user_id: @user.id, project_id: @project.id).includes(:task)
       @total = @timesheets.inject(0){|sum,e| sum += e.total_time }
       respond_to do |format|
         format.html
@@ -21,18 +20,18 @@ module Reports
 
     private
 
-    def set_time
-      @today = Time.zone.now.to_date
-      @day_selected = ( params[:date] ) ? DateTime.parse(params[:date]) : @today
-      @type = (params[:type]) ? params[:type] : "Weekly"
-      @tab = (params[:tab]) ? params[:tab] : "tab1"
-      if @type == "Weekly"
-        @beginning = @day_selected.at_beginning_of_week 
-        @end = @day_selected.at_end_of_week
-      elsif @type == "Monthly"
-        @beginning = @day_selected.at_beginning_of_month
-        @end = @day_selected.at_end_of_month
+      def set_time
+        @today = Time.zone.now.to_date
+        @day_selected = ( params[:date] ) ? DateTime.parse(params[:date]) : @today
+        @type = (params[:type]) ? params[:type] : "Weekly"
+        @tab = (params[:tab]) ? params[:tab] : "tab1"
+        if @type == "Weekly"
+          @beginning = @day_selected.at_beginning_of_week 
+          @end = @day_selected.at_end_of_week
+        elsif @type == "Monthly"
+          @beginning = @day_selected.at_beginning_of_month
+          @end = @day_selected.at_end_of_month
+        end
       end
-    end
   end
 end
