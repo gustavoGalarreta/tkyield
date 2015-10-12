d0e8ff9f7063200e0fdc08daaf06ece7c4cd3037
Eduardo Arenas <eduardo.arenas@tektonlabs.com>
2015-10-12 11:01:02 -0500

    solving conflicts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -80,6 +80,7 @@ class UsersController < DashboardController
     redirect_to users_path, notice: "Email sent successfully"
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
--- config/routes.rb
+++ config/routes.rb
@@ -22,9 +22,6 @@ Rails.application.routes.draw do
       get 'resend_confirmation', to: 'users#resend_confirmation', on: :member
       put 'archive', to: 'users#archive', on: :member
       put 'unarchive', to: 'users#unarchive', on: :member
-      get 'show_user_project', to: 'users#projects', on: :member
-      patch 'update_user_project', to: 'users#update_projects', on: :member
-      get 'resend_confirmation', to: 'users#resend_confirmation', on: :member
     end
     resources :tasks
     resources :clients
@@ -42,8 +39,6 @@ Rails.application.routes.draw do
       get 'list', to: 'reports#index'
       get 'dash', to: 'reports#dash'
       get 'tables', to: 'reports#tables'
-      get 'dash', to: 'reports#dash'
-      get 'list', to: 'reports#index'
       get 'clients_excel', to: 'reports#clients_excel'
       get 'projects_excel', to: 'reports#projects_excel'
       get 'collaborators_excel', to: 'reports#collaborators_excel'
